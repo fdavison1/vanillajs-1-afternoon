aafdcbedf3c69d3249c87965baa14abe3a1ec6bd
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-08 19:12:48 -0600

    cards final

--- Part2/index.js
+++ Part2/index.js
@@ -3,3 +3,9 @@
 const input1 = document.querySelector('#input1')
 
 const input2 = document.querySelector('#input2')
+
+function setCard(){
+    const card = document.getElementById(input1.value)
+    console.log(card)
+    card.style.color = input2.value
+}
